13555df915a91deb0eefd7e5329e8bbf937d8483
Saúl Arias Durán <saul_ariasd@hotmail.com>
2025-10-06 19:11:15 -0500

    legal agent info

--- src/pages/presentation/auth/Login.tsx
+++ src/pages/presentation/auth/Login.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useCallback, useContext, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import classNames from 'classnames';
-import { useFormik } from 'formik';
+import { FormikProps, useFormik } from 'formik';
 import PageWrapper from '../../../layout/PageWrapper/PageWrapper';
 import Page from '../../../layout/Page/Page';
 import Card, { CardBody } from '../../../components/bootstrap/Card';
@@ -15,6 +15,15 @@ import { getUserDataWithUsername } from '../../../common/data/userDummyData';
 import Spinner from '../../../components/bootstrap/Spinner';
 import { useLazyCheckEmailQuery } from '../../../store/api/auth.Api';
 import Select from '../../../components/bootstrap/forms/Select';
+import { getCountries } from '../../../utils/getCountries';
+
+interface RegisterFormValues {
+	nameLegalAgent: string;
+	ciLegalAgent: string;
+	prefixLegalAgent: string;
+	phoneLegalAgent: string;
+	addressLegalAgent: string;
+}
 
 interface ILoginHeaderProps {
 	isNewUser?: boolean;
@@ -98,6 +107,19 @@ const Login: FC<ILoginProps> = ({ isSignUp }) => {
 				}
 			}
 		},
+	});
+
+	const formikRegister = useFormik<RegisterFormValues>({
+		// enableReinitialize: true,
+		initialValues: {
+			nameLegalAgent: '',
+			ciLegalAgent: '',
+			prefixLegalAgent: '',
+			phoneLegalAgent: '',
+			addressLegalAgent: '',
+		},
+
+		onSubmit(values, formikHelpers) {},
 	});
 
 	const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -199,72 +221,7 @@ const Login: FC<ILoginProps> = ({ isSignUp }) => {
 								<form className='row g-4'>
 									{singUpStatus ? (
 										<>
-											<Card className='shadow-3d-dark p-4 mb-4'>
-												<CardBody className='g-2 row'>
-													<div className='col-12'>
-														<h5>Datos del representante legal</h5>
-													</div>
-													<div className='col-12 col-sm-6'>
-														<FormGroup
-															id='signup-name-legal-agent'
-															isFloating
-															label='Nombres y apellidos'>
-															<Input
-																type='email'
-																autoComplete='email'
-															/>
-														</FormGroup>
-													</div>
-													<div className='col-12 col-sm-6'>
-														<FormGroup
-															id='signup-ci-legal-agent'
-															isFloating
-															label='Identificación'>
-															<Input autoComplete='given-name' />
-														</FormGroup>
-													</div>
-													<div className='col-6 col-sm-3'>
-														<FormGroup
-															id='signup-ci-legal-agent'
-															label='Prefijo'
-															isFloating>
-															<Select
-																ariaLabel='Prefijo'
-																placeholder='Seleccione un prefijo'
-																title='Prefijo'
-																defaultValue='ruc'
-																list={[
-																	{
-																		value: 'ci',
-																		label: 'Cédula de identidad',
-																		text: 'CI',
-																	},
-																	{ value: 'ruc', label: 'RUC' },
-																]}
-															/>
-														</FormGroup>
-													</div>
-													<div className='col-12'>
-														<FormGroup
-															id='signup-surname'
-															isFloating
-															label='Your surname'>
-															<Input autoComplete='family-name' />
-														</FormGroup>
-													</div>
-													<div className='col-12'>
-														<FormGroup
-															id='signup-password'
-															isFloating
-															label='Password'>
-															<Input
-																type='password'
-																autoComplete='password'
-															/>
-														</FormGroup>
-													</div>
-												</CardBody>
-											</Card>
+											<LegalAgentInfo formikRegister={formikRegister} />
 											<div className='col-12'>
 												<Button
 													color='info'
@@ -365,3 +322,110 @@ const Login: FC<ILoginProps> = ({ isSignUp }) => {
 };
 
 export default Login;
+
+const LegalAgentInfo = ({
+	formikRegister,
+}: {
+	formikRegister: FormikProps<RegisterFormValues>;
+}) => {
+	const countryOptions = getCountries().map((country) => ({
+		value: country.dialCode,
+		label: `${country.flag} +${country.dialCode} ${country.name}`,
+		text: `+${country.dialCode} ${country.name}`,
+	}));
+	return (
+		<Card className='shadow-3d-dark p-4 mb-4'>
+			<CardBody className='g-2 row'>
+				<div className='col-12'>
+					<h5>Datos del representante legal</h5>
+				</div>
+				<div className='col-12 col-sm-6'>
+					<FormGroup id='nameLegalAgent' isFloating label='Nombres y apellidos'>
+						<Input
+							type='text'
+							autoComplete='nameLegalAgent'
+							value={formikRegister.values.nameLegalAgent}
+							isTouched={formikRegister.touched.nameLegalAgent}
+							invalidFeedback={formikRegister.errors.nameLegalAgent}
+							isValid={formikRegister.isValid}
+							onChange={formikRegister.handleChange}
+							onBlur={formikRegister.handleBlur}
+						/>
+					</FormGroup>
+				</div>
+				<div className='col-12 col-sm-6'>
+					<FormGroup id='ciLegalAgent' isFloating label='Identificación'>
+						<Input
+							type='text'
+							autoComplete='ciLegalAgent'
+							value={formikRegister.values.ciLegalAgent}
+							isTouched={formikRegister.touched.ciLegalAgent}
+							invalidFeedback={formikRegister.errors.ciLegalAgent}
+							isValid={formikRegister.isValid}
+							onChange={formikRegister.handleChange}
+							onBlur={formikRegister.handleBlur}
+						/>
+					</FormGroup>
+				</div>
+				<div className='col-6 col-sm-3'>
+					<FormGroup id='prefixLegalAgent' label='Prefijo' isFloating>
+						<Select
+							ariaLabel='Prefijo'
+							placeholder='Seleccione un prefijo'
+							title='Prefijo'
+							defaultValue='ruc'
+							list={countryOptions}
+							value={formikRegister.values.prefixLegalAgent}
+							isTouched={formikRegister.touched.prefixLegalAgent}
+							invalidFeedback={formikRegister.errors.prefixLegalAgent}
+							isValid={formikRegister.isValid}
+							onChange={formikRegister.handleChange}
+							onBlur={formikRegister.handleBlur}
+						/>
+					</FormGroup>
+				</div>
+				<div className='col-6 col-sm-3'>
+					<FormGroup id='phoneLegalAgent' isFloating label='Número de teléfono'>
+						<Input
+							type='tel'
+							autoComplete='phoneLegalAgent'
+							value={formikRegister.values.phoneLegalAgent}
+							isTouched={formikRegister.touched.phoneLegalAgent}
+							invalidFeedback={formikRegister.errors.phoneLegalAgent}
+							isValid={formikRegister.isValid}
+							onChange={(e) => {
+								const val = e.target.value.replace(/\D/g, ''); // elimina todo lo que no sea número
+								formikRegister.setFieldValue('phoneLegalAgent', val);
+							}}
+							onBlur={formikRegister.handleBlur}
+						/>
+					</FormGroup>
+				</div>
+				<div className='col-12 col-sm-6'>
+					<FormGroup id='addressLegalAgent' isFloating label='Dirección'>
+						<Input
+							type='text'
+							autoComplete='addressLegalAgent'
+							value={formikRegister.values.addressLegalAgent}
+							isTouched={formikRegister.touched.addressLegalAgent}
+							invalidFeedback={formikRegister.errors.addressLegalAgent}
+							isValid={formikRegister.isValid}
+							onChange={formikRegister.handleChange}
+							onBlur={formikRegister.handleBlur}
+						/>
+					</FormGroup>
+				</div>
+				<div className='col-12'>
+					<FormGroup id='signup-surname' isFloating label='Your surname'>
+						<Input autoComplete='family-name' />
+					</FormGroup>
+				</div>
+				<div className='col-12'>
+					<FormGroup id='signup-password' isFloating label='Password'>
+						<Input type='password' autoComplete='password' />
+					</FormGroup>
+				</div>
+			</CardBody>
+		</Card>
+	);
+};
---
+++ src/type/country-type.ts
@@ -0,0 +1,10 @@
+export interface CountryType {
+	name: string;
+	nameTranslations: any;
+	flag: string;
+	code: string;
+	dialCode: string;
+	regionCode?: string;
+	minLength: number;
+	maxLength: number;
+}
