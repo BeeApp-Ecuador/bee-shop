90deac9bd965280c6d5a7a154171e7e7044dd27e
Saúl Arias Durán <saul_ariasd@hotmail.com>
2025-10-21 11:19:27 -0500

    show buttons to select category

--- src/components/profile/FillProfile.tsx
+++ src/components/profile/FillProfile.tsx
@@ -6,10 +6,12 @@ import Button from '../bootstrap/Button';
 import FormGroup from '../bootstrap/forms/FormGroup';
 import Select from '../bootstrap/forms/Select';
 import { useGetCategoriesQuery } from '../../store/api/profileApi';
+import { ShopCategoryType } from '../../type/shop-category-type';
 
 const FillProfile = () => {
 	const { data } = useGetCategoriesQuery({});
-	const [categories, setCategories] = useState<[]>([]);
+	const [categories, setCategories] = useState<ShopCategoryType[]>([]);
+	const [selectedCategories, setSelectedCategories] = useState<ShopCategoryType[]>([]);
 
 	useEffect(() => {
 		if (data) {
@@ -29,28 +31,30 @@ const FillProfile = () => {
 				<WizardItem id='step1' title='Categoría y Tags'>
 					<Card>
 						<CardBody>
-							<div className='row g-4 align-items-center'>
-								<div className='col-xl'>
-									<div className='row g-4'>
-										<div className='col-auto'>
-											<Input
-												type='file'
-												autoComplete='photo'
-												ariaLabel='Upload image file'
-											/>
-										</div>
-										<div className='col-auto'>
-											<Button color='dark' isLight icon='Delete'>
-												Delete Avatar
-											</Button>
-										</div>
-										<div className='col-12'>
-											<p className='lead text-muted'>
-												Avatar helps your teammates get to know you.
-											</p>
-										</div>
-									</div>
-								</div>
+							<div className='d-flex flex-wrap justify-content-center gap-2'>
+								{categories.map((category) => {
+									const isSelected = selectedCategories.some(
+										(c) => c._id === category._id,
+									);
+
+									const handleToggle = () => {
+										setSelectedCategories((prev) =>
+											isSelected
+												? prev.filter((c) => c._id !== category._id)
+												: [...prev, category],
+										);
+									};
+
+									return (
+										<Button
+											key={category._id}
+											isOutline={!isSelected}
+											color='info'
+											onClick={handleToggle}>
+											{category.name}
+										</Button>
+									);
+								})}
 							</div>
 						</CardBody>
 					</Card>
