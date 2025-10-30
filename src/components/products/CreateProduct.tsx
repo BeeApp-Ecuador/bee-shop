d3947db3913b18c7eba13d07a67732a97611fd7b
Saúl Arias Durán <saul_ariasd@hotmail.com>
2025-10-30 10:30:06 -0500

    validations on options

--- src/components/products/CreateProduct.tsx
+++ src/components/products/CreateProduct.tsx
@@ -21,6 +21,8 @@ import { ProductType } from '../../type/product-type';
 import NewProductOption from './NewProductOption';
 import ListProductOptions from './ListProductOptions';
 import { OptionType } from '../../type/ItemOptionType';
+import showNotification from '../extras/showNotification';
+import Icon from '../icon/Icon';
 
 const CreateProduct = ({
 	setIsFillingProfile,
@@ -144,9 +146,102 @@ const CreateProduct = ({
 				},
 			],
 		},
+		validateOnChange: false,
+		validateOnBlur: false,
+
+		validate: (values) => {
+			const errors: Partial<Record<keyof OptionType, string>> = {};
+			if (values.title.trim().length === 0) {
+				errors.title = 'El título es obligatorio';
+			}
+			// verificas que el titulo no exista ya en las opciones temporales
+			const titleExists = temporaryOptions.find(
+				(option) => option.title.toLowerCase().trim() === values.title.toLowerCase().trim(),
+			);
+			if (titleExists) {
+				errors.title = 'Ya existe una opción con este título';
+			}
+			if (values.type === 'MULTIPLE') {
+				if (values.max <= 1) {
+					errors.max = 'El campo es obligatorio';
+				}
+			}
+			if (!values.isRequired) {
+				const filledItems = values.items.filter((item) => item.detail?.trim().length > 0);
+				if (filledItems.length < 2) {
+					console.log('aqui');
+					showNotification(
+						<span className='d-flex align-items-center'>
+							<Icon icon='Error' size='lg' className='me-1' />
+							<span>Error</span>
+						</span>,
+						'Agrega al menos dos ítems',
+						'danger',
+					);
+					errors.items = 'Agrega al menos dos ítems.';
+				}
+			}
+
+			if (values.isRequired) {
+				const filledItems = values.items.filter(
+					(item) =>
+						item.detail?.trim().length > 0 &&
+						item.priceWithVAT !== undefined &&
+						item.priceWithVAT !== null &&
+						item.priceWithVAT !== 0,
+				);
+				if (filledItems.length < 2) {
+					console.log('aqui');
+					showNotification(
+						<span className='d-flex align-items-center'>
+							<Icon icon='Error' size='lg' className='me-1' />
+							<span>Error</span>
+						</span>,
+						'Agrega al menos dos ítems con precio',
+						'danger',
+					);
+					errors.items = 'Agrega al menos dos ítems con precio.';
+				}
+				const invalidPriceItem = values.items.find(
+					(item) =>
+						item.detail.trim().length > 0 &&
+						(item.priceWithVAT === undefined ||
+							item.priceWithVAT === null ||
+							item.priceWithVAT === 0),
+				);
+				if (invalidPriceItem) {
+					showNotification(
+						<span className='d-flex align-items-center'>
+							<Icon icon='Error' size='lg' className='me-1' />
+							<span>Error</span>
+						</span>,
+						'Todos los ítems deben tener un precio válido',
+						'danger',
+					);
+					errors.items = 'Todos los ítems deben tener un precio válido.';
+				}
+			}
+
+			// verificar si existe un campo vacio en los items
+			console.log(errors);
+
+			return errors;
+		},
 		onSubmit: () => {
-			console.log('Options submitted:', formikOptions.values);
+			// limpiar los items que esten vacios
+			formikOptions.values.items = formikOptions.values.items.filter(
+				(item) => item.detail?.trim().length > 0,
+			);
 			setTemporaryOptions([...temporaryOptions, formikOptions.values]);
+			showNotification(
+				<span className='d-flex align-items-center'>
+					<Icon icon='Success' size='lg' className='me-1' />
+					<span>Éxito</span>
+				</span>,
+				'La opción ha sido agregada exitosamente.',
+				'success',
+			);
+			formikOptions.resetForm();
 		},
 	});
 
